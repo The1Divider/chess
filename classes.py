93f88f67ecac044465b89a8d64c6ce1210c8640a
i_4m_h4ck3rm4n <piunger@hotmail.com>
2021-01-27 00:03:00 -0800

    working on pawn class and possible moves

--- classes.py
+++ classes.py
@@ -2,8 +2,12 @@ WHITE, BLACK, EMPTY = "WHITE", "BLACK", "EMPTY"
 
 class Game:
     pieces: []
+    deadPieces: []
     def __init__(self):
-        self.pieces = [Rook(0, 0, BLACK), Knight(1, 0, BLACK), Bishop(2, 0, BLACK), Queen(3, 0, BLACK), King(4, 0, BLACK), Bishop(5, 0, BLACK), Knight(6, 0, BLACK), Rook(7, 0, BLACK), Pawn(0, 1, BLACK), Pawn(1, 1, BLACK), Pawn(2, 1, BLACK), Pawn(3, 1, BLACK), Pawn(4, 1, BLACK), Pawn(5, 1, BLACK), Pawn(6, 1, BLACK), Pawn(7, 1, BLACK), Pawn(0, 6, WHITE), Pawn(1, 6, WHITE), Pawn(2, 6, WHITE), Pawn(3, 6, WHITE), Pawn(4, 6, WHITE), Pawn(5, 6, WHITE), Pawn(6, 6, WHITE), Pawn(7, 6, WHITE), Rook(0, 7, WHITE), Knight(1, 7, WHITE), Bishop(2, 7, WHITE), Queen(3, 7, WHITE), King(4, 7, WHITE), Bishop(5, 7, WHITE), Knight(6, 7, WHITE), Rook(7, 7, WHITE)]
+        self.pieces = [Rook(0, 0, BLACK, self), Knight(1, 0, BLACK, self), Bishop(2, 0, BLACK, self), Queen(3, 0, BLACK, self), King(4, 0, BLACK, self), Bishop(5, 0, BLACK, self), Knight(6, 0, BLACK, self), Rook(7, 0, BLACK, self),
+                       Pawn(0, 1, BLACK, self), Pawn(1, 1, BLACK, self), Pawn(2, 1, BLACK, self), Pawn(3, 1, BLACK, self), Pawn(4, 1, BLACK, self), Pawn(5, 1, BLACK, self), Pawn(6, 1, BLACK, self), Pawn(7, 1, BLACK, self),
+                       Pawn(0, 6, WHITE, self), Pawn(1, 6, WHITE, self), Pawn(2, 6, WHITE, self), Pawn(3, 6, WHITE, self), Pawn(4, 6, WHITE, self), Pawn(5, 6, WHITE, self), Pawn(6, 6, WHITE, self), Pawn(7, 6, WHITE, self),
+                       Rook(0, 7, WHITE, self), Knight(1, 7, WHITE, self), Bishop(2, 7, WHITE, self), Queen(3, 7, WHITE, self), King(4, 7, WHITE, self), Bishop(5, 7, WHITE, self), Knight(6, 7, WHITE, self), Rook(7, 7, WHITE, self)]
 
     def getEmptySpaces(self):
         empty = [[i, j] for j in range(8) for i in range(8)]
@@ -14,27 +18,33 @@ class Game:
         return empty
     
     def getXY(self, x, y):
-        status = EMPTY
         for piece in self.pieces:
             if piece.x == x and piece.y == y:
-                status = piece
-        return status
+                return piece
+        return EMPTY
 
 class Piece:
     x: int
     y: int
     colour: str
-    def __init__(self, x, y, colour):
-        self.x, self.y, self.colour = x, y, colour
+    game: Game
+    def __init__(self, x, y, colour, game):
+        self.x, self.y, self.colour, self.game = x, y, colour, game
 
     def toString(self):
         return f"{self.colour} {type(self)} on ({self.x}, {self.y})."
 
 class Pawn(Piece):
-    def getAvailableMoves():
+    def getAvailableMoves(self):
         moves = []
         if (self.colour == BLACK):
-            pass
+            movesToCheck = [[self.x + 1, self.y],
+                            [self.x + 1, self.y + 1],
+                            [self.x + 1, self.y - 1]]
+            x = [self.game.getXY(x, y) for x, y in movesToCheck]
+
+            if self.x == 1:
+                pass # check for 2 spaces on first move
         else:
             pass
         return moves
