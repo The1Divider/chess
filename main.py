b87d37798dc865ba4c3e5fbbe07f9f2dd8a27db9
The_1_Divider <kmanprovo@gmail.com>
2021-06-01 13:40:56 -0600

    Tidied up code

--- main.py
+++ main.py
@@ -15,10 +15,9 @@ def main():
 
     move_status = None
     while move_status != MoveStatus.KING_IN_CHECKMATE:
+
         game.board.display_ascii_board()
         # game.board.to_fen(game)
-        print(game.en_passant_target)
-        print(game.current_player.colour)
 
         piece, move_position = None, None
         while piece is None or move_position is None:
@@ -29,15 +28,19 @@ def main():
 
             try:
                 piece = game.board.get(player_input[0])
-                if piece is None:
+
+                if piece is None or piece.colour != game.current_player.colour:
                     raise KeyError
+
             except KeyError:
                 print(f"Invalid piece selection: {player_input[0]}")
                 continue
 
             try:
+
                 if len(player_input) == 2:
                     move_position = player_input[1]
+
                 else:
                     move_position = input("Enter move position: ")
 
@@ -45,6 +48,7 @@ def main():
                 move_pos.set_with_notation(move_position)
 
             except InvalidPosition:
+
                 print(f"Invalid move position input: {move_position}")
                 move_position = None
                 continue
@@ -53,10 +57,15 @@ def main():
 
         if move_status == MoveStatus.INVALID_MOVE:
             print(f"Invalid move position: {move_position}")
+
         elif move_status == MoveStatus.PUTS_KING_IN_CHECK:
             print(f"Invalid move, puts king in check: {move_position}")
+
         elif move_status == MoveStatus.VALID_MOVE:
             game.next_turn()
+
+        else:
+            raise Exception(f"Move status: '{move_status}' received")
 
 
 if __name__ == "__main__":
